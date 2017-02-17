b51f18e0219b84e832804f0c0b59cb64974f260e
Axel Berlot <berlot83@yahoo.com.ar>
2017-02-16 21:40:40 -0300

    Subida de archivos por tarea en javascript y estilos del 17/02/2017

---
+++ tarea/comITProyecto-AxelB/WebContent/webapp/js/listado.js
@@ -0,0 +1,223 @@
+/*
+listadoCelulares = [
+		{
+			
+			"marca": "Motorola",
+			"modelo": "G 4 PLAY",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "8 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB"
+		},
+		{
+			"marca": "Motorola",
+			"modelo": "G 4 PLAY",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "8 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB"
+		},
+		{
+			"id": 1,
+			"marca": "Motorola",
+			"modelo": "G 4 PLAY",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "8 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		},
+		{
+			"id": 2,
+			"marca": "Samsung",
+			"modelo": "GALAXY J5 J510",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5.2,
+			"pantalla_resolucion": "720x1280",
+			"camara_resolucion": "13 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		},
+		{
+			"id": 3,
+			"marca": "Samsung",
+			"modelo": "GALAXY J7 J710",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5.5,
+			"pantalla_resolucion": "1280x720",
+			"camara_resolucion": "13 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		},
+		{
+			"id": 4,
+			"marca": "Huawei",
+			"modelo": "Y360D",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 4,
+			"pantalla_resolucion": "800x480",
+			"camara_resolucion": "5 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "2 Mpx",
+			"memoria_interna": "4 GB",
+			"flash": true
+		},
+		{
+			"id": 5,
+			"marca": "Lenovo",
+			"modelo": "VIBE K5",
+			"sistema_operativo": "Android",
+			"pantalla_pulgadas": 5,
+			"pantalla_resolucion": "800x480",
+			"camara_resolucion": "13 Mpx",
+			"camara_frontal": true,
+			"camara_frontal_res": "5 Mpx",
+			"memoria_interna": "16 GB",
+			"flash": true
+		}
+	];
+	Este es el original*/
+
+Celular1=[
+	{
+	"marca": 					"<font color='turquoise'><strong> 	Marca:					</strong></font> 		Sony",
+	"modelo": 					"<font color='turquoise'><strong>	Modelo:					</strong></font> 		Xperia M2",
+	"sistema_operativo": 		"<font color='turquoise'><strong>	Sist. op.:				</strong></font> 		Android Lolipop",
+	"pantalla_pulgadas": 		"<font color='turquoise'><strong>	Pantalla:				</strong></font> 		5",
+	"pantalla_resolucion": 		"<font color='turquoise'><strong>	Resolución:				</strong></font> 		1280x720",
+	"camara_resolucion": 		"<font color='turquoise'><strong>	Cámara res.:			</strong></font> 		8 Mpx",
+	"camara_frontal": 			"<font color='turquoise'><strong>	Cámara frontal:			</strong></font> 		Sí",
+	"camara_frontal_res": 		"<font color='turquoise'><strong>	Cámara front. res:		</strong></font> 		5 Mpx",
+	"memoria_interna": 			"<font color='turquoise'><strong>	Memoria int:			</strong></font> 		16 GB"
+}
+]
+
+Celular2=[
+	{
+	"marca": 					"<font color='green'><strong>		Marca:					</strong></font> 		Samsung",
+	"modelo": 					"<font color='green'><strong>		Modelo:					</strong></font> 		Core II",
+	"sistema_operativo": 		"<font color='green'><strong>		Sist. op.:				</strong></font> 		Android kitkat 4.1.2",
+	"pantalla_pulgadas": 		"<font color='green'><strong>		Pantalla:				</strong></font> 		4",
+	"pantalla_resolucion": 		"<font color='green'><strong>		Resolución:				</strong></font> 		1280x720",
+	"camara_resolucion": 		"<font color='green'><strong>		Cámara res.:			</strong></font> 		3 Mpx",
+	"camara_frontal": 			"<font color='green'><strong>		Cámara frontal:			</strong></font> 		Sí",
+	"camara_frontal_res": 		"<font color='green'><strong>		Cámara front. res:		</strong></font> 		5 Mpx",
+	"memoria_interna": 			"<font color='green'><strong>		Memoria int:			</strong></font> 		8 GB"
+}
+]
+
+Celular3=[
+	{
+	"marca": 					"<font color='yellow'><strong>		Marca:					</strong></font> 		Huawei",
+	"modelo": 					"<font color='yellow'><strong>		Modelo:					</strong></font> 		P8",
+	"sistema_operativo": 		"<font color='yellow'><strong>		Sist. op.:				</strong></font> 		Android candy",
+	"pantalla_pulgadas": 		"<font color='yellow'><strong>		Pantalla:				</strong></font> 		5,5",
+	"pantalla_resolucion": 		"<font color='yellow'><strong>		Resolución:				</strong></font> 		1280x720",
+	"camara_resolucion": 		"<font color='yellow'><strong>		Cámara res:				</strong></font> 		6 Mpx",
+	"camara_frontal": 			"<font color='yellow'><strong>		Cámara frontal:			</strong></font> 		Sí",
+	"camara_frontal_res": 		"<font color='yellow'><strong>		Cámara front. res:		</strong></font> 		8 Mpx",
+	"memoria_interna": 			"<font color='yellow'><strong>		Memoria int:			</strong></font> 		12 GB"
+}
+]
+
+Celular4=[
+	{
+	"marca": 					"<font color='violet'><strong>		Marca:					</strong></font> 		LG",
+	"modelo": 					"<font color='violet'><strong>		Modelo:					</strong></font>	 	Leon",
+	"sistema_operativo": 		"<font color='violet'><strong>		Sist. op.:				</strong></font> 		Android Lolipop",
+	"pantalla_pulgadas": 		"<font color='violet'><strong>		Pantlla:				</strong></font> 		2,5",
+	"pantalla_resolucion": 		"<font color='violet'><strong>		Resolución:				</strong></font> 		1280x720",
+	"camara_resolucion": 		"<font color='violet'><strong>		Cámara res:				</strong></font> 		8 Mpx",
+	"camara_frontal": 			"<font color='violet'><strong>		Cámara frontal:			</strong></font> 		Sí",
+	"camara_frontal_res": 		"<font color='violet'><strong>		Cámara front. res:		</strong></font> 		5 Mpx",
+	"memoria_interna": 			"<font color='violet'><strong>		Memoria int:			</strong></font> 		32 GB"
+}
+]
+
+
+function agregarElementos() 
+							{
+								var lista=document.getElementById("ulListado");  /*Reconoce el elemento html utilizado*/
+								
+								var linew= document.createElement("li");		/*Crea otro elemento dentro del elemento contenedor*/
+								
+								var contenido = document.createTextNode(listadoCelulares); 	/*Imprime algo dentro del elemento 'li' "*/
+							 
+								lista.appendChild(linew);
+										
+								linew.appendChild(contenido);
+}
+
+
+function agregarElementosStringify(){
+	
+	document.querySelector("ul").innerHTML = JSON.stringify(listadoCelulares);
+}
+
+
+
+
+/* Funciones por cada botón */
+function agregarElementosInnerHTML1(){
+	
+	var foo = Celular1.map(function(bar){
+		  return '<br>'+bar.marca+'<br>'+bar.modelo+'<br>'+bar.sistema_operativo+'<br>'+bar.pantalla_pulgadas+'<br>'+bar.pantalla_resolucion+'<br>'+bar.camara_frontal+'<br>'+bar.camara_frontal_res+'<br>'+bar.memoria_interna+'<br>'
+		})
+	
+		document.getElementById("ulListado1").innerHTML = foo;
+	
+}
+
+function agregarElementosInnerHTML2(){
+	
+	var foo = Celular2.map(function(bar){
+		  return '<br>'+bar.marca+'<br>'+bar.modelo+'<br>'+bar.sistema_operativo+'<br>'+bar.pantalla_pulgadas+'<br>'+bar.pantalla_resolucion+'<br>'+bar.camara_frontal+'<br>'+bar.camara_frontal_res+'<br>'+bar.memoria_interna+'<br>'
+		})
+	
+		document.getElementById("ulListado2").innerHTML = foo;
+	
+}
+
+function agregarElementosInnerHTML3(){
+	
+	var foo = Celular3.map(function(bar){
+		  return '<br>'+bar.marca+'<br>'+bar.modelo+'<br>'+bar.sistema_operativo+'<br>'+bar.pantalla_pulgadas+'<br>'+bar.pantalla_resolucion+'<br>'+bar.camara_frontal+'<br>'+bar.camara_frontal_res+'<br>'+bar.memoria_interna+'<br>'
+		})
+	
+		document.getElementById("ulListado3").innerHTML = foo;
+	
+}
+
+function agregarElementosInnerHTML4(){
+	
+	var foo = Celular4.map(function(bar){
+		  return '<br>'+bar.marca+'<br>'+bar.modelo+'<br>'+bar.sistema_operativo+'<br>'+bar.pantalla_pulgadas+'<br>'+bar.pantalla_resolucion+'<br>'+bar.camara_frontal+'<br>'+bar.camara_frontal_res+'<br>'+bar.memoria_interna+'<br>'
+		})
+	
+		document.getElementById("ulListado4").innerHTML = foo;
+	
+}
+
+
+function ocultarIngresar(){
+	document.getElementById('ingresar').style.display = 'none';
+}
+
+function mostrarIngresar(){
+	document.getElementById('ingresar').style.display = 'block';
+}
+
+
